dbfb10f63166ab3473ca97615e2d5f494ac0196c
rob <robcannonxyz@gmail.com>
2020-04-04 15:47:41 -0700

    adding port udp

--- examples/defaults/main.tf
+++ examples/defaults/main.tf
@@ -19,6 +19,14 @@ resource "aws_security_group" "this" {
     }
   }
 
+  ingress {
+    from_port = 1000
+    protocol  = "udp"
+    to_port   = 1000
+    cidr_blocks = [
+    "0.0.0.0/0"]
+  }
+
   egress {
     from_port = 0
     to_port   = 0
